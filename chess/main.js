ee41b61e7f47c93758a9180bbce1afad4daa27b7
MarrySad <mvrsad@gmail.com>
2019-01-21 17:17:42 +0700

    готово

--- chess/main.js
+++ chess/main.js
@@ -1,14 +1,14 @@
 function createChessField() {
     var newChessField = document.createElement("table");
     newChessField.className = "wholeBoard";
+    newChessField.setAttribute("align", "center");
     var isBlack;
-    //newChessField.appendChild(document.createElement("tr"));
     for (var i = 9; i > -1; i--) {
         newChessField.appendChild(document.createElement("tr"));
-        isBlack = i%2==0 ? 0 : 1;
+        isBlack = i % 2 == 0 ? 0 : 1;
         for (var j = '`'; j < 'j'; j = String.fromCharCode(j.charCodeAt(0) + 1)) {
             var td = document.createElement("td")
-            if ((i == 9 && j == '`') || (i == 0 && j == '`') || (i == 9 && j == 'i') || (i == 0 && j == 'i')) {td.className = "angle";}
+            if ((i == 9 && j == '`') || (i == 0 && j == '`') || (i == 9 && j == 'i') || (i == 0 && j == 'i')) { td.className = "angle"; }
             else if (i == 9) {
                 td.className = "upper range";
                 td.innerText = j;
@@ -26,7 +26,7 @@ function createChessField() {
                 td.innerText = i;
             }
             else {
-                td.className = i + j + " cell";
+                td.className = i + " " + j + " cell";
                 if (isBlack) {
                     td.classList.add("black");
                     isBlack = 0;
@@ -36,21 +36,26 @@ function createChessField() {
             }
             newChessField.lastChild.appendChild(td);
         }
-
     }
-
-
-
-
-
-
-
     document.body.appendChild(newChessField);
-    console.log(newChessField.childNodes);
 }
-/*
-for (var index = '`'; index < 'i'; index = String.fromCharCode(index.charCodeAt(0)+1)) {
 
-    console.log(index);
+function createCharacters() {
+    var arrCell = document.getElementsByClassName("cell");
+    for (var i = 0; i < arrCell.length; i++) {
+        if (i == 0 || i == 7) { arrCell[i].appendChild(document.createTextNode(characters[2])); }
+        if (i == 1 || i == 6) { arrCell[i].appendChild(document.createTextNode(characters[4])); }
+        if (i == 2 || i == 5) { arrCell[i].appendChild(document.createTextNode(characters[3])); }
+        if (i == 3) { arrCell[i].appendChild(document.createTextNode(characters[1])); }
+        if (i == 4) { arrCell[i].appendChild(document.createTextNode(characters[0])); }
+        if (i >= 8 && i <= 15) { arrCell[i].appendChild(document.createTextNode(characters[5])); }
+        if (i >= 48 && i <= 55) { arrCell[i].appendChild(document.createTextNode(characters[11])); }
+        if (i == 56 || i == 63) { arrCell[i].appendChild(document.createTextNode(characters[8])); }
+        if (i == 57 || i == 62) { arrCell[i].appendChild(document.createTextNode(characters[10])); }
+        if (i == 58 || i == 61) { arrCell[i].appendChild(document.createTextNode(characters[9])); }
+        if (i == 59) { arrCell[i].appendChild(document.createTextNode(characters[7])); }
+        if (i == 60) { arrCell[i].appendChild(document.createTextNode(characters[6])); }
+    }
 }
-*/
+
+var characters = ['♔', '♕', '♖', '♗', '♘', '♙', '♚', '♛', '♜', '♝', '♞', '♟'];
