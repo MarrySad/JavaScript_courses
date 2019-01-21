b623b4d9c458915b19591c011ddfe69b6e15db29
MarrySad <mvrsad@gmail.com>
2019-01-21 17:52:21 +0700

    Update main.js

--- chess/main.js
+++ chess/main.js
@@ -43,19 +43,19 @@ function createChessField() {
 function createCharacters() {
     var arrCell = document.getElementsByClassName("cell");
     for (var i = 0; i < arrCell.length; i++) {
-        if (i == 0 || i == 7) { arrCell[i].appendChild(document.createTextNode(characters[2])); }
-        if (i == 1 || i == 6) { arrCell[i].appendChild(document.createTextNode(characters[4])); }
-        if (i == 2 || i == 5) { arrCell[i].appendChild(document.createTextNode(characters[3])); }
-        if (i == 3) { arrCell[i].appendChild(document.createTextNode(characters[1])); }
-        if (i == 4) { arrCell[i].appendChild(document.createTextNode(characters[0])); }
-        if (i >= 8 && i <= 15) { arrCell[i].appendChild(document.createTextNode(characters[5])); }
-        if (i >= 48 && i <= 55) { arrCell[i].appendChild(document.createTextNode(characters[11])); }
-        if (i == 56 || i == 63) { arrCell[i].appendChild(document.createTextNode(characters[8])); }
-        if (i == 57 || i == 62) { arrCell[i].appendChild(document.createTextNode(characters[10])); }
-        if (i == 58 || i == 61) { arrCell[i].appendChild(document.createTextNode(characters[9])); }
-        if (i == 59) { arrCell[i].appendChild(document.createTextNode(characters[7])); }
-        if (i == 60) { arrCell[i].appendChild(document.createTextNode(characters[6])); }
+        if (i == 0 || i == 7) { arrCell[i].innerHTML = characters[8]; }
+        if (i == 1 || i == 6) { arrCell[i].innerHTML = characters[10]; }
+        if (i == 2 || i == 5) { arrCell[i].innerHTML = characters[9]; }
+        if (i == 3) { arrCell[i].innerHTML = characters[7]; }
+        if (i == 4) { arrCell[i].innerHTML = characters[6]; }
+        if (i >= 8 && i <= 15) { arrCell[i].innerHTML = characters[11]; }
+        if (i >= 48 && i <= 55) { arrCell[i].innerHTML = characters[5]; }
+        if (i == 56 || i == 63) { arrCell[i].innerHTML = characters[2]; }
+        if (i == 57 || i == 62) { arrCell[i].innerHTML = characters[4]; }
+        if (i == 58 || i == 61) { arrCell[i].innerHTML = characters[3]; }
+        if (i == 59) { arrCell[i].innerHTML = characters[1]; }
+        if (i == 60) { arrCell[i].innerHTML = characters[0]; }
     }
 }
 
-var characters = ['♔', '♕', '♖', '♗', '♘', '♙', '♚', '♛', '♜', '♝', '♞', '♟'];
+var characters = ['&#9812;', '&#9813;', '&#9814;', '&#9815;', '&#9816;', '&#9817;', '&#9818;', '&#9819;', '&#9820;', '&#9821;', '&#9822;', '&#9823;'];
