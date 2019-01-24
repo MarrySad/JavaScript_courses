39924b3c7afb6339f8c80947846d4ffcf5d46b82
MarrySad <mvrsad@gmail.com>
2019-01-24 14:41:16 +0700

    srhsh

---
+++ lesson_6/6.1/main.js
@@ -0,0 +1,9 @@
+var div = document.createElement("div");
+div.className = "previews";
+/*var preview = document.createElement("img");
+preview.className = "preview";
+for (var i = 1; i < 4; i++) {
+    preview.scr = "src/small/"+i+".img"
+    div.appendChild(preview)
+}*/
+document.body.appendChild(div);
