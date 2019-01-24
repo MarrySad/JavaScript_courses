6839840f07d6427a5cfc3adf8e063ec3fd5513a9
MarrySad <mvrsad@gmail.com>
2019-01-24 16:36:15 +0700

    выполнил первое задание

--- lesson_6/6.1/main.js
+++ lesson_6/6.1/main.js
@@ -1,9 +1,31 @@
-var div = document.createElement("div");
-div.className = "previews";
-/*var preview = document.createElement("img");
-preview.className = "preview";
+var previews = document.createElement("div");
+previews.className = "previews";
+
 for (var i = 1; i < 4; i++) {
-    preview.scr = "src/small/"+i+".img"
-    div.appendChild(preview)
-}*/
-document.body.appendChild(div);
+    var preview = document.createElement("img");
+    preview.className = "preview";
+    preview.setAttribute("src","src/small/" + i + ".jpg");
+    preview.setAttribute("onclick","viewImg(this.src)");
+    preview.style.margin = "20px";
+    previews.appendChild(preview);
+}
+
+document.body.appendChild(previews);
+document.body.appendChild(document.createElement("hr"));
+var output = document.createElement("div")
+output.className = "output";
+document.body.appendChild(output);
+
+
+function viewImg(src) {
+    document.querySelector(".output").innerHTML = "";
+    var img = document.createElement("img");
+    var arr = src.split('/');
+    img.setAttribute("src", "src/big/" + arr[arr.length-1]);
+    img.onerror = function() {
+        document.querySelector(".output").innerText = "Картинка не найдена :(";
+    }
+    document.querySelector(".output").appendChild(img);
+    
+    
+}
